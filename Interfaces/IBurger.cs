81cd56432d02d84d2991ec5c9250543e9a0cc25a
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-10-16 15:04:55 -0600

    burgers done

---
+++ Interfaces/IBurger.cs
@@ -0,0 +1,9 @@
+namespace Fall_BurgerShack.Interfaces
+{
+  public interface IBurger
+  {
+    string Name { get; set; }
+    string Description { get; set; }
+    string Price { get; set; }
+  }
+}
